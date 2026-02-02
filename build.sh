6e8790153692fd0b170eb6d18fbffdcee3d3cd76
exe.dev user <exedev@gold-grove.exe.xyz>
2026-02-02 22:46:34 +0000

    Add custom build script for Hugo Extended
    Co-authored-by: Shelley <shelley@exe.dev>

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Install Hugo Extended
+HUGO_VERSION="0.147.8"
+wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+tar -xzf hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+chmod +x hugo
+export PATH=$PWD:$PATH
+
+# Verify hugo extended
+./hugo version
+
+# Install npm dependencies
+npm install
+
+# Build site
+./hugo --minify
